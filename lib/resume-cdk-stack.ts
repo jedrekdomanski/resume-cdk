78dd0e72ead682402014a553b32d1001844a7338
jedrekdomanski <jedrek.domanski@gmail.com>
2022-10-31 23:20:20 +0100

    Add SNS topic, SQS and lambda

--- lib/resume-cdk-stack.ts
+++ lib/resume-cdk-stack.ts
@@ -5,7 +5,16 @@ import { S3BucketDeployment } from './s3_bucket_deployment';
 import { CloudFrontDistribution } from './cloud_front_distribution';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import { EMAIL_RESPONSE_BODY_KEY, EMAIL_RESPONSE_BODY_VALUE } from './consts';
+import {
+  EMAIL_RESPONSE_BODY_KEY,
+  EMAIL_RESPONSE_BODY_VALUE,
+  EMAIL_SUBSCRIPTION,
+  EMAIL_TOPIC_ARN,
+  EMAIL_TOPIC_NAME
+} from './consts';
+import { Queue as SQSQueue } from 'aws-cdk-lib/aws-sqs';
+import { Topic as SNSTopic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class ResumeCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,13 +26,32 @@ export class ResumeCdkStack extends cdk.Stack {
     // Cloudfront Distribution with origin as S3 bucket
     new CloudFrontDistribution(this, resumeBucket);
 
-    // Lambda function to send an email using SES
+    // SQS Queue
+    const queue = new SQSQueue(this, 'EmailSqsQueue', { fifo: false });
+
+    // SNS Topic
+    const topic = new SNSTopic(this, EMAIL_TOPIC_NAME, { displayName: 'Emails topic' });
+    topic.addSubscription(new EmailSubscription(EMAIL_SUBSCRIPTION));
+
+    // Lambda to long poll for new messages in SQS queue and publish the message to SNS topic
+    const sqsLambda = new lambda.Function(this, 'SQSLambda', {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      code: lambda.Code.fromAsset('lib/lambdas/email-lambda'),
+      environment: {
+        EMAIL_TOPIC_ARN: topic.topicArn
+      }
+    });
+
+    // Lambda function to send a message to SQS
     const mailerFunction = new lambda.Function(this, 'mailerFunction', {
       runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'index.handler',
       memorySize: 1024,
       timeout: cdk.Duration.seconds(3),
-      code: lambda.Code.fromAsset('resume-lambda'),
+      code: lambda.Code.fromAsset('lib/lambdas/sqs-lambda'),
       environment: {
         EMAIL_RESPONSE_BODY_KEY: EMAIL_RESPONSE_BODY_VALUE
       }
