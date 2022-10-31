6742acb5c4facac6f2ee96829fdd1a333c0b988e
jedrekdomanski <jedrek.domanski@gmail.com>
2022-10-31 22:18:37 +0100

    Updates to lambda function

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,3 +1,5 @@
 export const S3_BUCKET_NAME = 'website';
 export const S3_BUCKET_DEPLOYMENT_NAME = 'S3BucketDeployment';
 export const CLOUD_FRONT_DISTRIBUTION_NAME = 'resumeCfDistr';
+export const EMAIL_RESPONSE_BODY_KEY= 'response_body';
+export const EMAIL_RESPONSE_BODY_VALUE = 'Thank you for reaching out! I\'ll get back to you as soon as I can.';
--- lib/resume-cdk-stack.ts
+++ lib/resume-cdk-stack.ts
@@ -5,6 +5,7 @@ import { S3BucketDeployment } from './s3_bucket_deployment';
 import { CloudFrontDistribution } from './cloud_front_distribution';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { EMAIL_RESPONSE_BODY_KEY, EMAIL_RESPONSE_BODY_VALUE } from './consts';
 
 export class ResumeCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,15 +18,20 @@ export class ResumeCdkStack extends cdk.Stack {
     new CloudFrontDistribution(this, resumeBucket);
 
     // Lambda function to send an email using SES
-    const resFn = new lambda.Function(this, 'ResumeLambdaFunction', {
+    const mailerFunction = new lambda.Function(this, 'mailerFunction', {
       runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset('resume-lambda')
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      code: lambda.Code.fromAsset('resume-lambda'),
+      environment: {
+        EMAIL_RESPONSE_BODY_KEY: EMAIL_RESPONSE_BODY_VALUE
+      }
     });
 
-    // API Gateway REST API resource backed by "resFn" function.
+    // API Gateway REST API resource backed by "mailerFunction" function.
     const api = new apigw.LambdaRestApi(this, 'APIGateway', {
-      handler: resFn,
+      handler: mailerFunction,
       proxy: false
     });
 
